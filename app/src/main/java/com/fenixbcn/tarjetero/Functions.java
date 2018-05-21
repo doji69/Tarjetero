0329fd8e3303f4f6001369cd2b61dd73c7b21f54
javiercabregarcia <javi@autoescuelacorsa.com>
2018-05-21 22:52:52 +0200

    Update 21/05/2018

--- app/src/main/java/com/fenixbcn/tarjetero/Functions.java
+++ app/src/main/java/com/fenixbcn/tarjetero/Functions.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 public class Functions {
 
     public static SQLiteDatabase accessToDb (Context contex) {
-        
+
         DataBaseClass dbTajetero = new DataBaseClass(contex, "tarjetero", null, 1);
         SQLiteDatabase db = dbTajetero.getWritableDatabase();
 
