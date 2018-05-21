ca504055f842dff15b207104bbb5ca3eecf9676a
doji69 <39274985+doji69@users.noreply.github.com>
2018-05-21 22:55:16 +0200

    Update and rename functions.java to Functions.java

--- app/src/main/java/com/fenixbcn/tarjetero/Functions.java
+++ app/src/main/java/com/fenixbcn/tarjetero/Functions.java
@@ -3,7 +3,7 @@ package com.fenixbcn.tarjetero;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
-public class functions {
+public class Functions {
 
     public static SQLiteDatabase accessToDb (Context contex) {
 
